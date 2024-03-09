15efa1eb2361f588100f00480a17c0da2cc1a189
0laf3 <43992006+0laf3@users.noreply.github.com>
2024-03-09 12:48:34 +0100

    complited week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,46 @@
 
 /* Step 2 - Variables */
 
-
-
+let fullName = 'João Sebastião Capindissa';
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/IMG_20201102_111303.jpg";
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
+// const foodElement = document.getElementById('#food');
+const foodElement = document.querySelector('#food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
-
-
+// console.log(nameElement.textContent);
 
 /* Step 4 - Adding Content */
 
-
-
+// yearElement.textContent = currentYear;
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+// nameElement.textcontecnt = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}. He is a student in the CSE Class at BYUI`);
 
 
 
 /* Step 5 - Array */
+
+let favFoods = ['Bacalhau com natas', 'Calulu de carne seca', 'Cabidela'];
+// foodElement.textContent = `${favFoods}`;
+foodElement.innerHTML = favFoods;
+
+let newFood = 'Caldeirada';
+favFoods.push(newFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+
+favFoods.pop();
+
+foodElement.innerHTML += `<br>${favFoods}`;
 
 
 
